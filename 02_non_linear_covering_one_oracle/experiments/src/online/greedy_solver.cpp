2fdaed024130d442239814c443f2d80372b87712
Enikő Kevi <kevi.eniko@gmail.com>
2023-09-13 09:03:56 +0000

    Make the greedy solution integral in 02

--- 02_non_linear_covering_one_oracle/experiments/src/online/greedy_solver.cpp
+++ 02_non_linear_covering_one_oracle/experiments/src/online/greedy_solver.cpp
@@ -2,6 +2,7 @@
 
 
 #include "model/graph.h"
+#include "model/solution.h"
 #include "offline/frank_wolfe.h"
 
 
@@ -40,10 +41,34 @@ void GreedySolver::findInitialSolution(uint32_t s, uint32_t t)
 
 void GreedySolver::transformSolution(uint32_t r)
 {
+    uint32_t e, i, j;
+    uint32_t best_path = 0;
+    double best_objective = std::numeric_limits<double>::infinity();
+
+    Solution formatted_solution = Solution(_model, solution, _model.requests[r].source, _model.requests[r].target);
+
+    // Take the min cost path
+    if (formatted_solution.paths.size() > 1) {
+        for (uint32_t p = 0; p < formatted_solution.paths.size(); p++) {
+            double cost = 0.0;
+            for (uint32_t e_idx = 0; e_idx < formatted_solution.paths[p].size(); e_idx++) {
+                e = formatted_solution.paths[p][e_idx];
+                i = _model.graph.ID[e].i;
+                j = _model.graph.ID[e].j;
+                cost += _model.graph.A[i][j].getCost(cp_solution[e] + 1);
+            }
+            if (cost < best_objective) {
+                best_objective = cost;
+                best_path = p;
+            }
+        }
+    }
+
     uint32_t start = _model.graph.nb_edges + (r * _model.graph.nb_edges);
 
-    for (uint32_t e = 0; e < _model.graph.nb_edges; e++) {
-        cp_solution[(start + e)] = solution[e];
-        cp_solution[e] += solution[e];
+    for (uint32_t e_idx = 0; e_idx < formatted_solution.paths[best_path].size(); e_idx++) {
+        e = formatted_solution.paths[best_path][e_idx];
+        cp_solution[(start + e)] = 1.0;
+        cp_solution[e] += 1.0;
     }
 }
